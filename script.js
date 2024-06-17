fe734ec60903decec39f5b84b1b17a592188dd28
Mosquito brain <57737350+Mosquitobrain@users.noreply.github.com>
2024-06-17 18:36:56 -0400

    audio4

--- script.js
+++ script.js
@@ -67,6 +67,3 @@ setInterval(generateAndAddBouncingText, 100);
 setInterval(animateBouncingText, 0);
 
 generateAndAddBouncingText();
-const audio = new Audio('https://mosquitobrain.com/bussy_town.mp3');
-audio.loop = true;
-audio.play();
